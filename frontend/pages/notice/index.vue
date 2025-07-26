0c57f04518972e21a59f6a0681a43d9ef4638245
moony01 <mun01180@gmail.com>
2025-07-26 17:14:31 +0900

    중간 commit

---
+++ frontend/pages/notice/index.vue
@@ -0,0 +1,14 @@
+<script setup>
+const { data: notices } = await useFetch('/api/notice')
+</script>
+
+<template>
+  <div>
+    <h1>공지사항</h1>
+    <ul>
+      <li v-for="notice in notices" :key="notice.id">
+        {{ notice.title }} - {{ notice.createdAt }}
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ frontend/pages/notice/wirte.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+const title = ref('')
+const content = ref('')
+
+const submitNotice = async () => {
+  await fetch('/api/notice', {
+    method: 'POST',
+    body: JSON.stringify({ title: title.value, content: content.value }),
+  })
+  alert('등록 완료!')
+}
+</script>
+
+<template>
+  <div>
+    <h1>공지사항 작성</h1>
+    <input v-model="title" placeholder="제목" />
+    <textarea v-model="content" placeholder="내용" />
+    <button @click="submitNotice">등록</button>
+  </div>
+</template>
