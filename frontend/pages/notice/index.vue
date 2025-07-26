331336a081ee3473c72447ff1dafed27a165eaed
moony01 <mun01180@gmail.com>
2025-07-26 23:12:12 +0900

    backend수정

--- frontend/pages/notice/index.vue
+++ frontend/pages/notice/index.vue
@@ -5,6 +5,12 @@ const { data: notices } = await useFetch('/api/notice')
 <template>
   <div>
     <h1>공지사항</h1>
+    <!-- 글쓰기 버튼 -->
+    <div style="margin-bottom: 16px;">
+      <NuxtLink to="/notice/write">
+        <button>글쓰기</button>
+      </NuxtLink>
+    </div>
     <ul>
       <li v-for="notice in notices" :key="notice.id">
         {{ notice.title }} - {{ notice.createdAt }}
